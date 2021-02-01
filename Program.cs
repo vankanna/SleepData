8a87f839f445a8b0bec4da2e4d26e1ffd8057188
Sumin Rachwal <su2rachwal@gmail.com>
2021-02-01 14:18:38 -0600

    Create user Option

--- Program.cs
+++ Program.cs
@@ -5,8 +5,24 @@ namespace SleepData
     class Program
     {
         static void Main(string[] args)
-        {
+             {
             Console.WriteLine("Hello World!");
-        }
+            // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+
+            if (resp == "1")
+            {
+                // TODO: create data file
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+
+            }
+             }
     }
 }
